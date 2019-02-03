23d4ff65a09792f9d49d0a83d5596b79c27c3d36
liuwen <770335435@qq.com>
2019-02-03 15:21:52 +0800

    lw

--- src/main/resources/static/js/controller/managerCtrl.js
+++ src/main/resources/static/js/controller/managerCtrl.js
@@ -1,37 +1,38 @@
 app.service('manager',function ($rootScope,$http,chServer) {
     $rootScope.staffList = [];
     return {
+        //Get the employee list
         //获取员工列表
         getStaff:function () {
             $http.post("/camel/api/getStaffList" ,{
-            }).then(function (result) {  //正确请求成功时处理
+            }).then(function (result) {
                 $rootScope.staffList = result.data;
-                console.log("员工集合:"+JSON.stringify(result));
-            }).catch(function (result) { //捕捉错误处理
-                console.info(result);
+            }).catch(function (result) {
             });
         },
+        //Click the date to get the schedule information
         // 点击日期  获取日程信息
         allEventOnes :function (date, allDay, jsEvent, view,bid) {
+            //Get timestamp
                 // 获取时间戳
                 $rootScope.timeStamp = date._i;
-            console.log(date._i);
             var param = { timeStamp: date._i,bid:bid};
             $http.post("/camel/api/getDaySchedule",param ,{
-            }).then(function (result) {  //正确请求成功时处理
+            }).then(function (result) {
                 $rootScope.schedule = result.data;
-                console.log("sdada123:"+JSON.stringify(result));
-            }).catch(function (result) { //捕捉错误处理
-                console.info(result);
+            }).catch(function (result) {
             });
             $('#managerModal').modal('show');
         },
+        //Allocation schedule
         //分配日程
         mAddSchedule:function (bid) {
+            //Append a schedule for a single day
             //追加单独某天的日程
             var ms = {id:0,title:'',message: '',dateTime: '',endTime:'',infoId:$rootScope.timeStamp,userId:"",type:1,bid:bid}
             $rootScope.schedule.push(ms);
         },
+        //Delete allocation schedule
         // 删除分配日程
         mDeleteSchedule:function (indexs) {
             if($rootScope.schedule[indexs].id != 0){
@@ -39,6 +40,7 @@ app.service('manager',function ($rootScope,$http,chServer) {
             }
             $rootScope.schedule.splice(indexs,1);
         },
+        //Save the data
         // 保存数据
         mSaveSchedul:function () {
             for (var i =0;i<$rootScope.schedule.length;i++){
@@ -60,23 +62,21 @@ app.service('manager',function ($rootScope,$http,chServer) {
                 }
                 $rootScope.schedule[i].dateTime = d+c;
                 $rootScope.schedule[i].endTime = d+a;
-                console.log(times);
             }
             var param = {
                 time:$rootScope.timeStamp,
                 scheduleDel:$rootScope.scheduleDel,
                 schedule:$rootScope.schedule
             }
-            console.log(JSON.stringify(param))
             $http.post("/camel/api/saveCalendarSchdule",param ,{
-            }).then(function (result) {  //正确请求成功时处理
+            }).then(function (result) {
 
-            }).catch(function (result) { //捕捉错误处理
-                console.info(result);
+            }).catch(function (result) {
             });
             $rootScope.scheduleDel = [];
             $('#managerModal').modal('hide');
         },
+        //Close the window
         // 关闭窗口
         mCloseSchedul:function () {
             $rootScope.scheduleDel = [];
