0db22b2f2b77363aa2618503756cfdf647bbf3b5
liuwen <770335435@qq.com>
2019-02-03 15:18:32 +0800

    lw

--- src/main/resources/static/js/controller/modalCtrl.js
+++ src/main/resources/static/js/controller/modalCtrl.js
@@ -1,29 +1,34 @@
 app.service('modalsss',function ($rootScope,$http,chServer) {
-
+    //Click to see the process of selecting the date
     /*点击出现选择日期的历程*/
     $rootScope.schedule =[];
+    //All dates calendar schedule
     /*所有日期日历日程*/
     $rootScope.scheduleAll = [];
+    //Deleted collection
     //删除的集合
     $rootScope.scheduleDel = [];
     return {
 
+        //Click the date to get the schedule information
     // 点击日期  获取日程信息
         eventOnes :function (date, allDay, jsEvent, view,userId,bid) {
+            //Get timestamp
             // 获取时间戳
             $rootScope.timeStamp = date._i;
             console.log(date._i);
             var param = { timeStamp: date._i, userId: userId};
             $http.post("/camel/api/getDaySchedule",param ,{
-            }).then(function (result) {  //正确请求成功时处理
+            }).then(function (result) {
                 $rootScope.schedule = result.data;
                 console.log("sdada123:"+JSON.stringify(result));
-            }).catch(function (result) { //捕捉错误处理
+            }).catch(function (result) {
                 console.info(result);
             });
             $('#myModal').modal('show');
         },
 
+        //Delete the schedule
         //删除日程
         deleteProjs:function (indexs) {
             if($rootScope.schedule[indexs].id != 0){
@@ -32,14 +37,17 @@ app.service('modalsss',function ($rootScope,$http,chServer) {
             $rootScope.schedule.splice(indexs,1);
         },
 
+        //Add the schedule
         // 添加日程
         addSchedules: function (userId,bid) {
+            //Append a schedule for a single day
             //追加单独某天的日程
             var ms = {id:0,title:'',message: '',dateTime: '',endTime:'',infoId:$rootScope.timeStamp,userId:userId,type:1,bid:bid}
             $rootScope.schedule.push(ms);
             console.log(JSON.stringify($rootScope.schedule))
         },
 
+        //Save window replacement data
         // 保存窗口替换数据
         updateMss: function (userId) {
             // debugger;
@@ -61,26 +69,22 @@ app.service('modalsss',function ($rootScope,$http,chServer) {
                     a =  a + endTime;
                 }
                 $rootScope.schedule[i].dateTime = d+c;
-                console.log("开始："+$rootScope.schedule[i].dateTime)
                 $rootScope.schedule[i].endTime = d+a;
-                console.log("结束："+$rootScope.schedule[i].endTime)
             }
             var param = {
                 time:$rootScope.timeStamp,
                 scheduleDel:$rootScope.scheduleDel,
                 schedule:$rootScope.schedule
             }
-            console.log("sadasd:" + JSON.stringify(param))
             $http.post("/camel/api/saveCalendarSchdule",param ,{
-            }).then(function (result) {  //正确请求成功时处理
-
-            }).catch(function (result) { //捕捉错误处理
-                console.info(result);
+            }).then(function (result) {
+            }).catch(function (result) {
             });
             $rootScope.scheduleDel = [];
             $('#myModal').modal('hide');
         },
 
+        //Close the window to delete modified data
         //关闭窗口删除修改数据
         deleteMSs: function () {
             $rootScope.scheduleDel = [];
